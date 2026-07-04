6be673863e46afcc528a92b2a380a16e988d45a1
r4hulx <jdas54628@gmail.com>
2026-07-04 20:25:49 +0530

    Add Prototype Pattern demo

---
+++ Week1-DesignPatterns-Principles/DesignPatterns/PrototypeDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿Employee original = new Employee
+{
+    Name = "Rahul",
+    Department = "Engineering"
+};
+
+Employee cloned = original.Clone();
+
+Console.WriteLine("Original Employee");
+original.Display();
+
+Console.WriteLine();
+
+Console.WriteLine("Cloned Employee");
+cloned.Display();
